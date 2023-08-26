95ed01016770937b5d28631a5ed97a648d612d86
Andrew Marabante <andrewmarabante@gmail.com>
2023-08-26 15:34:50 -0700

    Added a getPossMoves which returns an obj to each array

--- src/board.js
+++ src/board.js
@@ -1,6 +1,6 @@
 
 let boardArray = [];
-class board 
+class cell 
 {
     constructor(x,y)
     {
@@ -8,9 +8,10 @@ class board
         this.y = y;
         this.start = null;
         this.end = null;
+        this.possmoves = getPossMoves(x,y);
     }
-
 }
+
 
 function setBoard ()
 {
@@ -18,8 +19,8 @@ function setBoard ()
     {
         for(j=1;j<9;j++)
         {
-           let cell = new board(i,j)
-           boardArray.push(cell)
+           let newCell = new cell(i,j)
+           boardArray.push(newCell)
         }
     }
 }
@@ -57,6 +58,28 @@ function setKnightEnd(x,y)
         console.log(boardArray)
     }
 }
+
+function getPossMoves(startx,starty)
+{
+    let possMoves = [];
+    let possx = [-2,-2,-1,-1,1,1,2,2];
+    let possy = [1,-1,2,-2,2,-2,1,-1];
+    for(let i=0;i<8;i++)
+    {
+        let newx = startx + possx[i];
+        let newy = starty + possy[i];
+        if ((newx>=1 && newx<=8) && (newy>=1 && newy<=8))
+        {
+            let newMove = {newx,newy};
+            possMoves.push(newMove);
+        }else{
+            possMoves.push(null);
+        }
+    }
+    return possMoves;
+
+}
+
 
 setBoard();
 console.log(boardArray)
